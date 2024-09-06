930d93a63166f162087aca315adcc1a2407f0412
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-09-06 14:33:01 -0300

    scroll suave tranformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,39 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = {
+        behavior: 'smooth',// scroll suave
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this)
+    //bind vai definir qual vai ser o this da funcao, no caos this.options
+}
 
-  function scrollToSection(event) {
+
+  scrollToSection(event) {
+   console.log(this.options)
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+    section.scrollIntoView(this.options);
+ }
+  
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+    link.addEventListener('click', this.scrollToSection);
+  });
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  init() {
+    if(this.linksInternos.length)
+    this.addLinkEvent();
+    return this;
+ }
+  
 }
+
+
