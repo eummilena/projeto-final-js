d75e9d7944611b1078fd31cbe4bc8dc52f24eae6
Milena Mota <80923819+eummilena@users.noreply.github.com>
2024-09-27 16:11:48 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
-
+import SlideNav from "./modules/slide.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -40,6 +40,11 @@ funcionamento.init();
 
 
 
+
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
 
 fetchAnimais();
 
